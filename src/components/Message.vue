620e46a3302165857212d372c979bda36367d4e4
m2 <467057463@qq.com>
2021-08-28 22:29:21 +0800

    study vue3 setup

---
+++ src/components/Message.vue
@@ -0,0 +1,15 @@
+<template lang='pug'>
+.message(@click="emit('test', 'test')")
+  slot
+  span {{props.title}}
+</template>
+
+<script setup lang="ts">
+// import { defineProps, defineEmits } from 'vue';
+const props = defineProps(['title', 'content']);
+const emit = defineEmits(['test']);
+
+
+console.log(props)
+console.log(emit)
+</script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,18 @@
 <template lang="pug">
 el-button(@click="count++") {{count}}
+Message(
+  title="titleabcdefg"
+  @test="test"
+)
+  b hello component
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
+import Message from '../components/Message.vue';
 const count = ref(0);
 
+function test($evnet){
+  console.log('close', $evnet)
+}
 </script>
